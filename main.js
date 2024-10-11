558ee6b5c292594152e769636f655f7d5c2171cd
android8624 <9704re@gmail.com>
2024-10-11 12:03:20 -0300

    .

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ function criaCartao (categoria,pergunta,resposta){
     let cartao = document.createElement('article');
     cartao.className='cartao';
     cartao.innerHTML=`
-    <div class="conteudo-cartao">
+   <div>
+<div class="conteudo-cartao">
     <h3>categoria</h3>
+</div>
 <div class="pergunta-cartao">
     <p>resposta</p>
 </div>
