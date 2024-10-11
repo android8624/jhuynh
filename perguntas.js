c274ed0d4fca6fc56f9b7202b0803642559646b0
android8624 <9704re@gmail.com>
2024-10-11 11:42:22 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,40 @@
+riaCartao('Matemática', 'Qual é a formula de Bhaskara?',' -b ± √(b² – 4ac)/2a')
+
+criaCartao('Fisica', 'Como se calcula a energia cinetica de um objeto?','Usamos a fórmula Ec=mv²/2')
+
+criaCartao('História', 'Qual era a justificativa que Vargas deu para dar um golpe de estado?','Ele utilizou comk pretesto, o plano cohen, que era um suposto plano comunista para tomar o poder')
+
+criaCartao('Química', 'Como calcular a velocidade média de consumo?','V=Δn/Δt')
+
+criaCartao('Física', 'Como calcular a velociadade média?',' usamos a fórmula, Vm=Δns/Δt')
+
+criaCartao('Física', 'Qual é a formula do Movimento Uniforme(MU)?','S=S0+V.∆t')
+
+criaCartao('Química', 'Como se calcula a Energia de Ativação(EAT)?','utiliza-se a fórmula EAT = ECA - HR')
+
+criaCartao('Matemática','Qual é a fórmula da função afim?','utiliza-se a fórmula f(x) ax+b')
+
+criaCartao('Matemática','Qual é a fórmula usada para calcular o seno?','utiliza-se a fórmula CO/H')
+
+criaCartao('Matemática','Qual é a fórmula usada para calcular o cosseno?','utiliza-se a fórmula CA/H')
+
+criaCartao('Matemática','Qual é a fórmula usada para calcular a Tangente?','utiliza-se a fórmula CO/CA')
+
+criaCartao('História','O que foi a guerra do contestado?','foi um conflito armado ocorrido de outubro de 1912 a agosto de 1916, que teve como partes beligerantes posseiros e pequenos proprietários de terras contra os governos dos estados de Santa Catarina e Paraná, além do Governo Federal brasileiro')
+
+criaCartao('História', 'O que foi o "Queremismo"?','Foi um movimento popular que visava trazer Getúlio Vargas de volta ')
+criaCartao('História', 'O que foi a política do café com leite"?','foi a alternância entre um governo de um candidato de Minas Gerais e outro de São Paulo')
+
+criaCartao('Biologia', 'Qual é a expressão dos genes?','É o conjunto de peocessos que permite às células codificarem as proteínas para o desenvolvimento de seus processos vitais')
+
+criaCartao('Biologia', 'As proteínas do ADN são chamadas____','Histonas')
+
+criaCartao('Física', 'Como é chamada a estrutura das proteínas do ADN?','nucleossoma')
+
+criaCartao('Biologia', 'O que é anagenese?','Compreende processos pelos quais uma característica surge ou se modifica numa população ao longo do tempo, sendo responsável pelas “novidades evolutivas”')
+
+criaCartao('Língua portuguesa', 'Quais são os dois tipos de gramática?','Normativa(norma culta) e descritiva(descreve a analisa lingua usada por um grupo de pessoas')
+
+criaCartao('Geografia', 'O que édemocracia?','É um regime de governo onde o poder de tomar decisões importantes é com os cidadãos').
+
+criaCartao('Biologia', 'Qual  é a principal ideia da teoria de Lamark','Que as característicasadquiridas durante a vida de um indivíduo, são passadas para seus descendentes')
