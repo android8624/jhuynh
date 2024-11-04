6440c5d1d506764379b890bc0865c33db7521e76
android8624 <9704re@gmail.com>
2024-11-04 08:15:52 -0300

    .

--- projeto2.js
+++ projeto2.js
@@ -16,7 +16,7 @@ function criaCartao (categoria,pergunta,resposta){
 
 container.appendChild(cartao);
 }
-function mostrarResposta (card){
+function mostraResposta (card){
     const resposta = card.querySelector('.resposta-cartao')
     if(resposta.style.display === 'none'){
         resposta.style.display = 'block';
