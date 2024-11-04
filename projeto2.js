8ef32438877a799204e1fc61f63f113eabf14dca
android8624 <9704re@gmail.com>
2024-11-04 07:58:24 -0300

    .

--- projeto2.js
+++ projeto2.js
@@ -11,36 +11,16 @@ function criaCartao (categoria,pergunta,resposta){
 <div class="resposta-cartao">
     <p>${resposta}</p>
 </div></div>
-    `
-
-    let respostaEstaVisivel
-    function viraCartao(){
-        respostaEstaVisivel = !respostaEstaVisivel;
-        cartao.classList.toggle('active',respostaEstaVisivel);
-    }
-cartao.addEventListener('click',viraCartao)
-    container.appendChild(cartao);
+    ` 
 }
-function criaCartao (categoria,pergunta,resposta){
-    let container =document.getElementById('container');
-    let cartao = document.createElement('article');
-    cartao.className='cartao';
-    cartao.innerHTML=`
-<div class="conteudo-cartao">
-    <h3>${categoria}</h3>
-<div class="pergunta-cartao">
-    <p>${pergunta}</p>
-</div>
-<div class="resposta-cartao">
-    <p>${resposta}</p>
-</div></div>
-    `
 
-    let respostaEstaVisivel
-    function viraCartao(){
-        respostaEstaVisivel = !respostaEstaVisivel;
-        cartao.classList.toggle('active',respostaEstaVisivel);
+
+function.mostrarResposta(card){
+    const resposta = document.querySelector('.resposta-cartao')
+
+    if(resposta.style.display === 'none'){
+        resposta.style.display = 'block';
     }
-cartao.addEventListener('click',viraCartao)
-    container.appendChild(cartao);
+   else(resposta.style.display === 'none')
+
 }
